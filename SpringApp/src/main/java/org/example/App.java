3d94e0d27583844d9edac6055bf933909e2269a8
loshan2001 <loshansp2001@gmail.com>
2025-08-28 16:06:55 +0530

    learning about contructor injection and setter injection

--- SpringApp/src/main/java/org/example/App.java
+++ SpringApp/src/main/java/org/example/App.java
@@ -13,6 +13,7 @@ public class App
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         Dev obj = (Dev) context.getBean("dev");
+        System.out.println(obj.getAge());
         obj.build();
     }
 
--- SpringApp/src/main/java/org/example/Dev.java
+++ SpringApp/src/main/java/org/example/Dev.java
@@ -1,7 +1,28 @@
 package org.example;
 
 public class Dev {
+    private int age;
+
+    public Laptop getLaptop() {
+        return laptop;
+    }
+
+    public void setLaptop(Laptop laptop) {
+        this.laptop = laptop;
+    }
+
+    private Laptop laptop;
+    public Dev(Laptop laptop){
+        this.laptop = laptop;
+    }
     public void build(){
         System.out.println("working on awesome project");
+        laptop.compile();
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age = age;
     }
 }
